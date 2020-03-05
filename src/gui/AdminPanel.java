ccff943a171f94b2b7e1b767459a3d305bc10fa8
Geetika Gupta <22guptageetika@gmail.com>
2020-03-05 15:51:13 -0800

    Bill;

---
+++ src/gui/AdminPanel.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+
+public class AdminPanel {
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button2;
+}
